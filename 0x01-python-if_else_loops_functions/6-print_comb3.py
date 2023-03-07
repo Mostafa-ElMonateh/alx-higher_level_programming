c3c7b12cad559fa06e6cb4598ab141b2db0283dd
Mostafa ElMonateh <ElMonateh@gmail.com>
2023-03-07 10:54:26 -0800

    format not foramt

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for lDigit in range(0, 10):
         if lDigit == 8 and rDigit == 9:
             print("{}{}".format(lDigit, rDigit))
         else:
-            print("{}{}".foramt(lDigit, rDigit), end=", ")
+            print("{}{}".format(lDigit, rDigit), end=", ")
