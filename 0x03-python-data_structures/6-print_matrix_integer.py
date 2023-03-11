609a53a2f3e555a7c3fed2107c599c3fc4b05462
Mostafa ElMonateh <ElMonateh@gmail.com>
2023-03-11 04:54:36 -0800

    updating 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" " if col != row[-1] else "$")
+    if matrix:
+        for row in matrix:
+            for value in row:
+                if value == row[0]:
+                    print("{:d}".format(value), end="")
+                else:
+                    print(" {:d}".format(value), end="")
+            print()
+    else:
         print()
